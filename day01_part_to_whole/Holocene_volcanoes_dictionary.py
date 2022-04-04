059461d9f9d38de0b225b59a7c94562462f8540e
Roanna Victorio <roannav@users.noreply.github.com>
2022-04-03 22:02:43 -0700

    add data

---
+++ day01_part_to_whole/Holocene_volcanoes_dictionary.py
@@ -0,0 +1,22 @@
+dict_num_volcano = {
+    "United States": 161,
+    "Japan": 122,
+    "Indonesia": 121,
+    "Russia": 117,
+    "Chile": 92,
+    "Ethiopia": 53,
+    "Papua New Guinea":	47,
+    "Philippines": 38,
+    "Mexico": 37,
+    "Argentina": 36,
+    "Ecuador": 35,
+    "Iceland": 34,
+    "New Zealand": 25,
+    "Canada": 24,
+    "Guatemala": 23,
+    "Tonga": 21,
+    "Kenya": 21,
+    "El Salvador": 20,
+    "France": 20,
+    "Antarctica": 19
+}
